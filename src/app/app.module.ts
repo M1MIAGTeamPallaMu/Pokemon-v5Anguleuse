65c9b31d9fa1459a37560453247550812b51f7af
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-02-01 11:40:41 +0100

    app component

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule} from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {DataService} from './dataservice/data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokedexComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+
+  ],
+  providers: [ DataService, HttpClient],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
