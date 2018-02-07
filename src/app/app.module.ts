e2a37fc640fc4976595e570f256368cf46375cbe
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-02-07 10:37:54 +0100

    updated added DataService to imports and HttpClientModule to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
@@ -13,8 +14,7 @@ import {DataService} from './dataservice/data.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
-
+    HttpClientModule,
   ],
   providers: [ DataService, HttpClient],
   bootstrap: [AppComponent]
